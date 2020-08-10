6b173065c771a8893cb72c02c1a25df18f27e7e3
webdenway <clthbbd@gmail.com>
2020-08-11 01:44:33 +0600

    mapp

--- map-filter-find.js
+++ map-filter-find.js
@@ -21,3 +21,8 @@ const numbers = [3, 4, 5, 6, 7, 8];
 // console.log(output);
 
 // ====filter===
+// const bigger = numbers.filter(x => x > 5);
+const bigger = numbers.find(x => x > 5);
+
+
+console.log(bigger);
