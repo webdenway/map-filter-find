76cd4985f0467da3683076f107a846d2e0447ef7
webdenway <clthbbd@gmail.com>
2020-08-11 01:26:51 +0600

    map

--- map-filter-find.js
+++ map-filter-find.js
@@ -1,9 +1,23 @@
 const numbers = [3, 4, 5, 6, 7, 8];
-const output = [];
+// const output = [];
 
-for (let i = 0; i < numbers.length; i++) {
-    const product = numbers[i];
-    const result = product * product;
-    output.push(result);
-}
-console.log(output);
+// for (let i = 0; i < numbers.length; i++) {
+//     const product = numbers[i];
+//     const result = product * product;
+//     output.push(result);
+// }
+//==== map
+// numbers.map(function(product, index, array) {
+//     console.log(product, index, array);
+// })
+
+// const output = numbers.map(function(product) {
+//     return product * product;
+// })
+
+
+// const output = numbers.map(x => x * x);
+
+// console.log(output);
+
+// ====filter===
