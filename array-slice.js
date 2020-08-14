c8be11f4646ae6798e02fe19e8e322f3a345fb9a
webdenway <clthbbd@gmail.com>
2020-08-15 03:57:13 +0600

    array slice splice

---
+++ array-slice.js
@@ -0,0 +1,11 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// const part = nums.slice(2, 5);
+
+
+// const removed = nums.splice(2, 5, 33, 34, 35);
+// console.log(removed);
+// console.log(part);
+// console.log(nums);
+
+const together = nums.join("/")
+console.log(together);
